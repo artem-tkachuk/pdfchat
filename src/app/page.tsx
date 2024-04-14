734e18d5e288f43dc60c58422e8967b8e082dfb0
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-04-13 22:33:00 -0700

    Get started on landing headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,24 @@ export default function Home() {
           PDFChat is now public!
         </p>
       </div>
+      <h1 className="
+        max-w-4xl 
+        text-5xl 
+        font-bold 
+        md:text-6xl 
+        lg:text-7xl"
+      >
+        Chat with your <span className="text-blue-600">documents</span>
+      </h1>
+      <p className="
+        mt-5 
+        max-w-prose 
+        text-zinc-700
+        sm:text-lg"
+      >
+        PDF Chat allows you to have conversations with any PDF document. <br></br>
+        Simply upload your file and start asking questions right away!
+      </p>
     </MaxWidthWrapper>
   );
 }
